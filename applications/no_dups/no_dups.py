8449402b4e96245515b12166a253dfb0c4fe43b4
John Oblenda <joandong2@gmail.com>
2020-10-06 21:15:37 -0400

    first 4c:/Python/Python385/python.exe

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,17 @@
+
+
 def no_dups(s):
     # Your code here
-
+    dict = {}
+    for i in s.split():
+        if i not in dict:
+            dict[i] = i
+    return " ".join(dict.values())
 
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
-    print(no_dups("hello hello"))
+    # print(no_dups(""))
+    # print(no_dups("hello"))
+    # print(no_dups("hello hello"))
     print(no_dups("cats dogs fish cats dogs"))
-    print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    # print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
